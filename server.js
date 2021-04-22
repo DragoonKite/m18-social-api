51f55169dca970a8c6c2270519a9bfe1ce6a097b
DragoonKite <frank.depaolo@yahoo.com>
2021-04-22 11:36:18 -0500

    added some boiler plate code and some file structure

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//logs queries 
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
